983a30c885444bde3a743932399223cbf59278c0
ZacTohZY <zactoh0503@gmail.com>
2023-12-29 00:04:52 +0800

    Initial Commit

--- __init__.py
+++ __init__.py
@@ -8,6 +8,16 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+
+@app.route('/forum')
+def forum():
+    return render_template('forum.html')
+
+
 if __name__ == '__main__':
     app.run()
 
