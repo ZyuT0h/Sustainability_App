8ae1839c6c1f66429e1e0067d298ee9c4d9ee39c
Rykkel <220993G@mymail.nyp.edu.sg>
2023-12-29 06:40:55 +0800

    Initial Commit

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,10 +7,42 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/homeUser')
+def home_user():
+    return render_template('homeUser.html')
+
+@app.route('/homeAdmin')
+def home_admin():
+    return render_template('homeAdmin.html')
+
+@app.route('/userLogin')
+def user_login():
+    return render_template('userLogin.html')
+
+
+@app.route('/adminLogin')
+def admin_login():
+    return render_template('adminLogin.html')
+
 
 @app.route('/shop')
 def shop():
     return render_template('shop.html')
+
+
+@app.route('/addProduct')
+def add_products():
+    return render_template('addProduct.html')
+
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+
+@app.route('/manageInventory')
+def manage_inventory():
+    return render_template('manageInventory.html')
 
 
 @app.route('/forum')
@@ -20,4 +52,3 @@ def forum():
 
 if __name__ == '__main__':
     app.run()
-
