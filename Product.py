e903a8cf5283591dedfeb9495a7d9ae3b297b12a
Rykkel <220993G@mymail.nyp.edu.sg>
2023-12-30 20:26:02 +0800

    Initial Commit

--- Product.py
+++ Product.py
@@ -2,20 +2,23 @@ class Product:
     count_id = 0
 
     def __init__(self, product_name, category, stock, price, description):
-        product.count_id += 1
-        self.__product_id = product.count_id
+        Product.count_id += 1
+        self.__product_id = Product.count_id
         self.__product_name = product_name
+        # self.__product_image = product_image
         self.__category = category
         self.__stock = stock
         self.__price = price
         self.__description = description
-
 
     def set_product_id(self, product_id):
         self.__product_id = product_id
 
     def set_product_name(self, product_name):
         self.__product_name = product_name
+        
+    # def set_product_image(self, product_image):
+    #     self.__product_image = product_image
 
     def set_category(self, category):
         self.__category = category
@@ -34,12 +37,19 @@ class Product:
 
     def get_product_name(self):
         return self.__product_name
+    
+    # def get_product_image(self):
+    #     return self.__product_image
 
     def get_category(self):
-        return self.__cetegory
+        return self.__category
 
     def get_stock(self):
         return self.__stock
+    
+    def get_price(self):
+        return self.__price
 
     def get_description(self):
         return self.__description
+    
--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+from Forms import CreateProductForm
+import shelve, Product
 
 app = Flask(__name__)
 
@@ -7,13 +9,16 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
 @app.route('/homeUser')
 def home_user():
     return render_template('homeUser.html')
 
+
 @app.route('/homeAdmin')
 def home_admin():
     return render_template('homeAdmin.html')
+
 
 @app.route('/userLogin')
 def user_login():
@@ -30,19 +35,56 @@ def shop():
     return render_template('shop.html')
 
 
-@app.route('/addProduct')
-def add_products():
-    return render_template('addProduct.html')
-
-
 @app.route('/cart')
 def cart():
     return render_template('cart.html')
 
 
+@app.route('/addProduct', methods=['GET', 'POST'])
+def add_product():
+    create_product_form = CreateProductForm(request.form)
+    if request.method == 'POST' and create_product_form.validate():
+        products_dict = {}
+        db = shelve.open('product.db', 'c')
+
+        try:
+            products_dict = db['Products']
+        except:
+            print('Error in retrieving Products from product.db.')
+
+        product = Product.Product(create_product_form.product_name.data,
+                                  create_product_form.category.data,
+                                  create_product_form.stock.data,
+                                  create_product_form.price.data,
+                                  create_product_form.description.data)
+
+        products_dict[product.get_product_id()] = product
+        db['Products'] = products_dict
+
+        # Test codes
+        products_dict = db['Products']
+        product = products_dict[product.get_product_id()]
+        print(product.get_product_name(), "was stored in product.db successfully with product_id ==", product.get_product_id())
+        
+        db.close()
+
+        return redirect(url_for('home_admin'))
+    return render_template('addProduct.html', form=create_product_form)
+
+
 @app.route('/manageInventory')
 def manage_inventory():
-    return render_template('manageInventory.html')
+    products_dict = {}
+    db = shelve.open('product.db', 'r')
+    products_dict = db['Products']
+    db.close()
+    
+    products_list = []
+    for key in products_dict:
+        product = products_dict.get(key)
+        products_list.append(product)
+    
+    return render_template('manageInventory.html', count=len(products_list), products_list=products_list)
 
 
 @app.route('/forum')
