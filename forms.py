978297a9ec0e4127f49c83b947c56b7d1ded88a3
Harini312821 <harini280905@gmail.com>
2024-01-02 23:26:33 +0800

    registration pg

--- forms.py
+++ forms.py
@@ -1,9 +1,11 @@
 from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators
+from wtforms.fields import EmailField, DateField
 
-class CreateUserForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')],
-default='')
-    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
-    remarks = TextAreaField('Remarks', [validators.Optional()])
+
+class CreateProductForm(Form):
+    product_name = StringField('Product Name:', [validators.Length(min=1, max=150),validators.DataRequired()])
+    category = SelectField('Category:', [validators.DataRequired()], choices=[('','Select'),('Coasters', 'Coasters'),
+                                                                             ('Tote bags', 'Tote bags'),('Pouches', 'Pouches')],default='')
+    stock = StringField('Stock:', [validators.Length(min=1, max=150), validators.DataRequired()])
+    price = StringField('Price:', [validators.Length(min=1, max=150), validators.DataRequired()])
+    description = TextAreaField('Description:',[validators.Optional()])
