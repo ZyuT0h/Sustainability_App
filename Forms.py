b2e8d76a9f6d7b098c341bcc217e638bf6f403c5
Rykkel <220993G@mymail.nyp.edu.sg>
2024-01-06 01:02:58 +0800

    Initial Commit

--- Forms.py
+++ Forms.py
@@ -1,6 +1,6 @@
 from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators
 from wtforms.fields import EmailField, DateField
-
+d
 
 class CreateProductForm(Form):
     product_name = StringField('Product Name:', [validators.Length(min=1, max=150),validators.DataRequired()])
