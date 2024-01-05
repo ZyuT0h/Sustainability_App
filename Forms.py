3e1ad32fdab635da69f96a899e52012aaed200f6
Rykkel <220993G@mymail.nyp.edu.sg>
2024-01-06 01:00:55 +0800

    Initial Commit

--- Forms.py
+++ Forms.py
@@ -6,6 +6,6 @@ class CreateProductForm(Form):
     product_name = StringField('Product Name:', [validators.Length(min=1, max=150),validators.DataRequired()])
     category = SelectField('Category:', [validators.DataRequired()], choices=[('','Select'),('Coasters', 'Coasters'),('Tote bags', 'Tote bags'),('Pouches',
                                                                                             'Pouches')], default='')
-    stock = StringField('Stockssssss:', [validators.Length(min=1, max=150), validators.DataRequired()])
+    stock = StringField('Stocks:', [validators.Length(min=1, max=150), validators.DataRequired()])
     price = StringField('Price:', [validators.Length(min=1, max=150), validators.DataRequired()])
     description = TextAreaField('Description:', [validators.Optional()])
